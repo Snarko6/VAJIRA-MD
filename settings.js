b369548f784c292eadd9b858e157be6b75c64c50
Snarko6 <165910445+Snarko6@users.noreply.github.com>
2024-04-14 13:41:21 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,14 +22,14 @@ global.APIs = {
 
 // Other
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = 'ᴠᴀᴊɪʀᴀ ʀᴀᴛʜɴᴀʏᴀᴋᴀ';
-global.owner = ['94719199757','94703475691'];
-global.premium = ['94719199757','94703475691'];
+global.ownername = '𝒔𝒏𝒂𝒓𝒌𝒐>333‘';
+global.owner = ['201095499866','201095499866'];
+global.premium = ['201095499866','201095499866'];
 global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
 global.author = '𝙑𝘼𝙅𝙄𝙍𝘼 𝙈𝘿';
 global.caption = ' 👨‍💻 ＶＡＪＩＲＡ ＭＤ V3 👨‍💻';
 global.sessionName = 'session';
-global.prefa = ['!', '.'];
+global.prefa = ['!', '201095499866'];
 global.sp = '✪';
 global.mess = {
     banned: '`You are *Banned* fron using commands!`',
